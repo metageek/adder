d03826f45e134fdb13b2639191e7475bfa9d5b86
John Stracke <francis+github@thibault.org>
2010-03-25 11:32:12 -0400

    More work on Gomer.  Starting to test what I've got.

--- adder/gomer.py
+++ adder/gomer.py
@@ -1,6 +1,40 @@
 # A structured internal representation; basically an annotated form of
 #  Adder itself, with macros expanded.  Gets converted to Pyle.
 
+import itertools,re,pdb
+
+class NoCommonAncestor(Exception):
+    def __str__(self):
+        return 'The two scopes have no common ancestor.'
+
+class NotConstant(Exception):
+    def __init__(self,expr):
+        Exception.__init__(self,expr)
+
+    def __str__(self):
+        return '%s is not a constant.' % self.args
+
+class Undefined(Exception):
+    def __init__(self,varRef):
+        Exception.__init__(self,varRef)
+
+    def __str__(self):
+        return 'Undefined variable: %s' % self.args
+
+class DefinedAfterUse(Exception):
+    def __init__(self,var,initExpr,accesses):
+        Exception.__init__(self,var,initExpr,accesses)
+
+    def __str__(self):
+        return 'Variable defined after use: %s defined as %s after used at %s' % self.args
+
+class Redefined(Exception):
+    def __init__(self,var,initExpr,oldVarEntry):
+        Exception.__init__(self,var,initExpr,oldVarEntry)
+
+    def __str__(self):
+        return 'Variable redefined: %s defined as %s after being defined at %s' % self.args
+
 # Information known about a variable
 class VarEntry:
     def __init__(self,name,initExpr):
@@ -13,7 +47,7 @@ class VarEntry:
 
     def constValue(self):
         if not self.neverModified:
-            raise NotConstant()
+            raise NotConstant(None)
         return self.initExpr.constValue()
 
 # Table of vars  known in a lexical scope
@@ -22,6 +56,23 @@ class Scope:
         self.parent=parent
         self.localDefs={}
         self.varAccesses={}
+
+    def isDescendant(self,other):
+        cur=self
+        while cur:
+            if cur==other:
+                return True
+            cur=cur.parent
+        return False
+
+    def commonAncestor(self,other):
+        if self.isDescendant(other):
+            return other
+        if other.isDescendant(self):
+            return other
+        if not self.parent:
+            raise NoCommonAncestor()
+        return self.parent.commonAncestor(other)
 
     def __contains__(self,var):
         return (self.isLocal(var)
@@ -41,7 +92,7 @@ class Scope:
             return self.localDefs[varRef.name]
         if self.parent:
             return self.parent[varRef]
-        raise Undefined(varRef.name,varRef)
+        raise Undefined(varRef)
 
     def addDef(self,var,initExpr):
         if var in self.varsAccessed:
@@ -71,7 +122,19 @@ class Expr:
         self.scope=scope
 
     def constValue(self):
-        raise NotConstant()
+        raise NotConstant(self)
+
+    # Return the scope, ancestral to self.scope, which contains
+    #  all var definitions upon which this expr depends.  For
+    #  a constant, this is None.
+    def scopeRequired(self):
+        pass
+
+    def varRefs(self):
+        return []
+
+    def isPureIn(self,containingScope):
+        return False
 
 class Constant(Expr):
     def __init__(self,scope,value):
@@ -80,6 +143,12 @@ class Constant(Expr):
 
     def constValue(self):
         return self.value
+
+    def scopeRequired(self):
+        return None
+
+    def isPureIn(self,containingScope):
+        return True
 
 class Call(Expr):
     def __init__(self,scope,f,args):
@@ -90,9 +159,32 @@ class Call(Expr):
     def constValue(self):
         fv=self.f.constValue()
         if not fv.isPure():
-            raise NotConstant()
-        argVs=map(lambda a: a.constValue(),self.args)
+            raise NotConstant(self)
+        argVs=list(map(lambda a: a.constValue(),self.args))
         return fv(*argVs)
+
+    def scopeRequired(self):
+        required=self.f.scopeRequired()
+        for arg in self.args:
+            required=required.commonAncestor(self.args.scopeRequired())
+        return required
+
+    def varRefs(self):
+        for var in self.f.varRefs():
+            yield var
+
+        for arg in self.args:
+            for var in arg.varRefs():
+                yield var
+
+    def isPureIn(self,containingScope):
+        fv=self.f.constValue()
+        if not fv.isPure():
+            return False
+        for arg in self.args:
+            if not arg.isPureIn(constValue):
+                return False
+        return True
 
 class VarRef(Expr):
     def __init__(self,scope,name):
@@ -100,7 +192,30 @@ class VarRef(Expr):
         self.name=name
 
     def constValue(self):
-        return self.scope[self].constValue()
+        try:
+            return self.scope[self].constValue()
+        except NotConstant: # the one from VarEntry will have None for its expr
+            raise NotConstant(self)
+
+    def scopeRequired(self):
+        cur=self.scope
+        while cur:
+            if cur.isLocal(self.name):
+                return cur
+        raise Undefined(self)
+
+    def varRefs(self):
+        return [self]
+
+    def isPureIn(self,containingScope):
+        if self.scope.isDescendant(containingScope):
+            return True
+
+        try:
+            self.constValue()
+        except NotConstant:
+            return False
+        return True
 
 class Function:
     def isPure(self):
@@ -114,10 +229,25 @@ class NativeFunction(Function):
     def isPure(self):
         return self.pure
 
-
 class UserFunction(Function):
+    # The fExpr should be the (define) or (lambda) that created this function.
     def __init__(self,fExpr):
         self.fExpr=fExpr
+        assert isinstance(fExpr,Call)
+        assert isinstance(fExpr.f,VarRef)
+        assert fExpr.f.name in {'define','lambda'}
+        assert fExpr.args
+        assert isinstance(fExpr.args[0],list)
+        for arg in fExpr.args[0]:
+            assert isinstance(arg,VarRef)
+        self.argList=fExpr.args[0]
+        self.bodyExprs=fExpr.args[1:]
 
     def isPure(self):
-        
+        if not self.bodyExprs:
+            return True
+        localScope=self.bodyExprs[0].scope
+        for expr in self.bodyExprs:
+            if not expr.isPureIn(localScope):
+                return False
+        return True
--- tests/test-gomer.py
+++ tests/test-gomer.py
@@ -4,12 +4,57 @@ import unittest,pdb,sys,os
 from adder.gomer import *
 from adder.common import Symbol as S
 
+class VarEntryTestCase(unittest.TestCase):
+    def testConstValueSuccess(self):
+        var=VarEntry('fred',Constant(None,17))
+        val=var.constValue()
+        assert val==17
+            
+    def testConstValueFailure(self):
+        var=VarEntry('fred',Constant(None,17))
+        var.markModified()
+        try:
+            val=var.constValue()
+            assert False
+        except NotConstant:
+            return
+            
 class ScopeTestCase(unittest.TestCase):
-    def testSimple(self):
-        pass
+    def testIsDescendant(self):
+        scope1=Scope(None)
+        scope2=Scope(None)
+        scope1_1=Scope(scope1)
+        scope1_1_1=Scope(scope1_1)
+        scope2_1=Scope(scope2)
+        scope2_2=Scope(scope2)
+
+        assert scope1.isDescendant(scope1)
+        assert scope1_1.isDescendant(scope1)
+        assert scope1_1_1.isDescendant(scope1)
+
+        assert not scope1.isDescendant(scope1_1)
+        assert scope1_1.isDescendant(scope1_1)
+        assert scope1_1_1.isDescendant(scope1_1)
+
+        assert not scope1.isDescendant(scope1_1_1)
+        assert not scope1_1.isDescendant(scope1_1_1)
+        assert scope1_1_1.isDescendant(scope1_1_1)
+
+        assert not scope1.isDescendant(scope2)
+        assert not scope1_1.isDescendant(scope2)
+        assert not scope1_1_1.isDescendant(scope2)
+
+        assert not scope1.isDescendant(scope2_1)
+        assert not scope1_1.isDescendant(scope2_1)
+        assert not scope1_1_1.isDescendant(scope2_1)
+
+        assert not scope1.isDescendant(scope2_2)
+        assert not scope1_1.isDescendant(scope2_2)
+        assert not scope1_1_1.isDescendant(scope2_2)
 
 suite=unittest.TestSuite(
-    ( unittest.makeSuite(ScopeTestCase,'test'),
+    ( unittest.makeSuite(VarEntryTestCase,'test'),
+      unittest.makeSuite(ScopeTestCase,'test'),
      )
     )
 
