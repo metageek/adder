7ecfe384b677a95c7575fa50571d3422559e7ecb
John Stracke <francis@barrayar.local.thibault.org>
2010-04-07 21:34:04 -0400

    Testing that defconst is not just a #define.

--- tests/test-gomer-pyle.py
+++ tests/test-gomer-pyle.py
@@ -183,19 +183,19 @@ class GomerToPythonTestCase(CompilingTestCase):
 
     def testCallDefconst(self):
         self.compile([S('defconst'),S('x'),
-                      [S('*'),9,7]])
+                      [S('f'),9,7]])
         assert self.exprPython=='x'
-        assert self.pythonFlat=="""x=9*7
+        assert self.pythonFlat=="""x=f(9,7)
 """
 
     def testUseConst(self):
         self.compile([S('begin'),
                       [S('defconst'),S('x'),
-                      [S('*'),9,7]],
+                      [S('f'),9,7]],
                       S('x')])
         scratch1=S('#<gensym-scratch #1>').toPython()
         assert self.exprPython==scratch1
-        assert self.pythonFlat=="""x=9*7
+        assert self.pythonFlat=="""x=f(9,7)
 %s=x
 """ % scratch1
 
