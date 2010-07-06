9eea68863a11005b101b90fc4b651c49dcfe51c1
John Stracke <francis@thibault.org>
2010-07-05 20:55:30 -0400

    Taking a command-line arg.

--- view-prof.py
+++ view-prof.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
 
-import pstats
+import pstats,sys
 
-p=pstats.Stats('adderprof')
+if len(sys.argv)>1:
+    assert len(sys.argv)==2
+    f=sys.argv[1]
+else:
+    f='adderprof'
+
+p=pstats.Stats(f)
 p.strip_dirs().sort_stats('cumulative').print_stats()
